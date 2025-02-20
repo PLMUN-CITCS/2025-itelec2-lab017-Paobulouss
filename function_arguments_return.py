c584c17f4ec3d9dcb43167621ba57165f190ad9e
Christian Paolo Marfil <marfilchristianpaolo_bsit@plmun.edu.ph>
2025-02-20 08:52:20 +0800

    function_arguments_return.py

--- function_arguments_return.py
+++ function_arguments_return.py
@@ -1,9 +1,9 @@
 import math
 
 def circle_area(radius):
-  """Calculate and return the area of a circle given its radius."""
-area = math.pi * (radius ** 2)
-    return area
+    """Calculate and return the area of a circle given its radius."""
+    area = math.pi * (radius ** 2) 
+    return area 
 
 radius_value = 5
 area_result = circle_area(radius_value)
